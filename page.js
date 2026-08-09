5d8a1b5cfd9a7960be25a885a8685d8e38d59cc8
Abinash Ray <abinashray53@gmail.com>
2026-08-09 11:27:12 +0530

    Add files via upload

---
+++ page.js
@@ -0,0 +1,9 @@
+var R=require("../../chunks/ssr/[turbopack]_runtime.js")("server/app/_global-error/page.js")
+R.c("server/chunks/ssr/[root-of-the-server]__1_yrzun._.js")
+R.c("server/chunks/ssr/0oif_next_dist_esm_build_templates_app-page_0due9f7.js")
+R.c("server/chunks/ssr/[root-of-the-server]__0qcz2g1._.js")
+R.c("server/chunks/ssr/[root-of-the-server]__1cnvxo2._.js")
+R.c("server/chunks/ssr/0oif_next_dist_client_components_builtin_global-error_1ofbkgx.js")
+R.c("server/chunks/ssr/_next-internal_server_app__global-error_page_actions_0zi5s8-.js")
+R.m(5395)
+module.exports=R.m(5395).exports
